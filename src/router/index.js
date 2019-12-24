60fc5eee4685796a281f247f37ff72ed13a77ba2
好久不见 <287099738@qq.com>
2019-12-24 16:48:02 +0800

    在“master”提交

--- src/router/index.js
+++ src/router/index.js
@@ -6,30 +6,6 @@ Vue.use(Router)
 /* Layout */
 import Layout from '@/layout'
 
-/**
- * Note: sub-menu only appear when route children.length >= 1
-
- *
- * hidden: true                   if set true, item will not show in the sidebar(default is false)
- * alwaysShow: true               if set true, will always show the root menu
- *                                if not set alwaysShow, when item has more than one children route,
- *                                it will becomes nested mode, otherwise not show the root menu
- * redirect: noRedirect           if set noRedirect will no redirect in the breadcrumb
- * name:'router-name'             the name is used by <keep-alive> (must set!!!)
- * meta : {
-    roles: ['admin','editor']    control the page roles (you can set multiple roles)
-    title: 'title'               the name show in sidebar and breadcrumb (recommend set)
-    icon: 'svg-name'             the icon show in the sidebar
-    breadcrumb: false            if set false, the item will hidden in breadcrumb(default is true)
-    activeMenu: '/example/list'  if set path, the sidebar will highlight the path you set
-  }
- */
-
-/**
- * constantRoutes
- * a base page that does not have permission requirements
- * all roles can be accessed
- */
 export const constantRoutes = [
   {
     path: '/login',
@@ -90,6 +66,32 @@ export const constantRoutes = [
         name: 'storageWaste',
         component: () => import('@/views/storageWaste/index'),
         meta: { title: '入库废品', icon: 'tree' }
+      }
+    ]
+  },
+
+  {
+    path: '/outWaste',
+    component: Layout,
+    children: [
+      {
+        path: 'outWaste',
+        name: 'outWaste',
+        component: () => import('@/views/outWaste/index'),
+        meta: { title: '出库废品', icon: 'nested' }
+      }
+    ]
+  },
+
+  {
+    path: '/acceptanceWaste',
+    component: Layout,
+    children: [
+      {
+        path: 'acceptanceWaste',
+        name: 'acceptanceWaste',
+        component: () => import('@/views/acceptanceWaste/index'),
+        meta: { title: '验收废品', icon: 'link' }
       }
     ]
   },
